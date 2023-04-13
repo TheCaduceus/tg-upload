7095d696298a118baa370842ecc3f00e4eefb3fe
Sourcery AI <>
2023-04-13 17:57:53 +0000

    'Refactored by Sourcery'

--- tg-upload.py
+++ tg-upload.py
@@ -75,9 +75,8 @@ if args.api_id and args.api_hash:
   elif not args.phone and not args.bot:
     if not Path(f"{args.profile}.session").exists():
       raise ValueError ("Given profile is not yet initialized! provide phone number or bot token to initialize.")
-elif not args.api_id or not args.api_hash:
-  if not Path(f'{args.profile}.session').exists() and not args.login_string:
-    raise ValueError("Given profile is not yet initialized! provide API_ID and API_HASH to initialize.")
+elif not Path(f'{args.profile}.session').exists() and not args.login_string:
+  raise ValueError("Given profile is not yet initialized! provide API_ID and API_HASH to initialize.")
 
 if args.phone:
   client = Client(
@@ -227,26 +226,25 @@ with client:
             print(f"An error occured!\n{error_code}")
         else:
           print(f"[Dir] -> {filename}")
-  else:
-    if Path(args.path).is_file():
-      try:
-        filename = args.filename or PurePath(args.path).name
-        if args.prefix:
-          filename = args.prefix + filename
-        client.send_document(chat_id, args.path, progress=upload_progress, caption=args.caption, force_document=True, file_name=filename, thumb=args.thumb, disable_notification=args.silent)
-        Path(args.path).unlink(missing_ok=True) if args.delete_on_done else None
-      except Exception as error_code:
+  elif Path(args.path).is_file():
+    try:
+      filename = args.filename or PurePath(args.path).name
+      if args.prefix:
+        filename = args.prefix + filename
+      client.send_document(chat_id, args.path, progress=upload_progress, caption=args.caption, force_document=True, file_name=filename, thumb=args.thumb, disable_notification=args.silent)
+      Path(args.path).unlink(missing_ok=True) if args.delete_on_done else None
+    except Exception as error_code:
+        print(f"An error occured!\n{error_code}")
+  elif Path(args.path).is_dir():
+    for _path in Path(args.path).glob("**/*") if args.recursive else Path(args.path).glob("*"):
+      if Path(_path).is_file():
+        try:
+          filename = PurePath(_path).name
+          if args.prefix:
+            filename = args.prefix + filename
+          client.send_document(chat_id, _path, progress=upload_progress, caption=args.caption, force_document=True, file_name=filename, thumb=args.thumb, disable_notification=args.silent)
+          Path(_path).unlink(missing_ok=True) if args.delete_on_done else None
+        except Exception as error_code:
           print(f"An error occured!\n{error_code}")
-    elif Path(args.path).is_dir():
-      for _path in Path(args.path).glob("**/*") if args.recursive else Path(args.path).glob("*"):
-        if Path(_path).is_file():
-          try:
-            filename = PurePath(_path).name
-            if args.prefix:
-              filename = args.prefix + filename
-            client.send_document(chat_id, _path, progress=upload_progress, caption=args.caption, force_document=True, file_name=filename, thumb=args.thumb, disable_notification=args.silent)
-            Path(_path).unlink(missing_ok=True) if args.delete_on_done else None
-          except Exception as error_code:
-            print(f"An error occured!\n{error_code}")
-        else:
-          print(f"[Dir] -> {filename}")
+      else:
+        print(f"[Dir] -> {filename}")
